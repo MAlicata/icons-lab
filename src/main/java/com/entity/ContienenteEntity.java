9e0fc1b4d1476a4655509d4d4b5ef9a786d145a7
MAlicata <matiasalicata@gmail.com>
2021-11-22 19:22:19 -0300

    entity

---
+++ src/main/java/com/entity/ContienenteEntity.java
@@ -0,0 +1,20 @@
+package com.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="continente")
+@Getter
+@Setter
+
+public class ContienenteEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String imagen;
+    private String denominacion;
+
+}
